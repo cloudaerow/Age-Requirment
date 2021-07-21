d3131f63dd9139cbb95d1096c3fa1f9b32b8f6c4
cloud peacock <willrpeacock@gmail.com>
2021-07-21 16:01:07 -0400

    Add project files.

---
+++ Input and Operators/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main() 
+{
+	int x;
+	int enter;
+#pragma region
+	cout << "root@ubuntu: ";
+	Sleep(250);
+	cout << ".";
+	Sleep(250);
+	cout << "/";
+	Sleep(250);
+	cout << "A";
+	Sleep(250);
+	cout << "g";
+	Sleep(250);
+	cout << "e";
+	Sleep(250);
+	cout << "R";
+	Sleep(250);
+	cout << "e";
+	Sleep(250);
+	cout << "q";
+	Sleep(250);
+	cout << "u";
+	Sleep(250);
+	cout << "i";
+	Sleep(250);
+	cout << "r";
+	Sleep(250);
+	cout << "e";
+	Sleep(250);
+	cout << "m";
+	Sleep(250);
+	cout << "e";
+	Sleep(250);
+	cout << "n";
+	Sleep(250);
+	cout << "t";
+	Sleep(250);
+	cout << ".";
+	Sleep(250);
+	cout << "s";
+	Sleep(250);
+	cout << "h";
+	Sleep(250);
+	cout << "\n";
+#pragma endregion
+	Sleep(2000);
+	cout << "Please enter your age: ";
+	cin >> x;
+	cout << "Thanks! Please wait a few seconds";
+	Sleep(1000);
+	cout << ".";
+	Sleep(1000);
+	cout << ".";
+	Sleep(1000);
+	cout << ".";
+	Sleep(1000);
+	cout << "\nYour age has been recorded. Thank you";
+	cout << "\nPress y and enter to close the software: ";
+	cin >> enter;
+	cout << "Goodbye!";
+	Sleep(1000);
+	return 0;
+}
